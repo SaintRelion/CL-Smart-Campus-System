cd1671b3d4b21233aad778f1ff8d952f3c461145
SaintRelion <saintrelion@gmail.com>
2026-03-08 12:26:43 +0800

    env

--- src/data-access-config.tsx
+++ src/data-access-config.tsx
@@ -4,7 +4,7 @@ import {
   setGlobalMode,
 } from "@saintrelion/data-access-layer";
 
-export const API_URL = "http://127.0.0.1:8000/";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 setAuthLibClientAppName("smartcampus");
 setDataAccessLayerClientAppName("smartcampus");
