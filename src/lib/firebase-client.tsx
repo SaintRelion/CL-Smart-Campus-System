442743bb7551c2272e00e2c25a845036131d2486
SaintRelion <saintrelion@gmail.com>
2026-09-25 13:15:17 +0800

    removed creds

--- src/lib/firebase-client.tsx
+++ src/lib/firebase-client.tsx
@@ -2,19 +2,16 @@ import { initializeFirebaseAuth } from "@saintrelion/auth-lib";
 import { initializeFirestore } from "@saintrelion/data-access-layer";
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REMOVED_FIREBASE_API_KEY_1",
-  authDomain: "REMOVED_FIREBASE_AUTH_DOMAIN_1",
-  projectId: "REMOVED_FIREBASE_PROJECT_ID_1",
-  storageBucket: "REMOVED_FIREBASE_PROJECT_ID_1.firebasestorage.app",
-  messagingSenderId: "REMOVED_FIREBASE_MESSAGING_SENDER_ID_1",
-  appId: "REMOVED_FIREBASE_APP_ID_1",
-  measurementId: "REMOVED_FIREBASE_MEASUREMENT_ID_2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 initializeFirestore(app);
